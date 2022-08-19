ef79f33659367cf710a6d69da4a158a993e80a7f
haxxxer <q66p@hotmail.com>
2022-08-19 19:54:21 +0300

    render html not text

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const PORT = process.env.PORT || 3000
 
 
 app.get('/myCloud/backupCode/instagram', (req,res) => {
-
-    res.end("<html><head><title>Recovery Codes</title></head><body><img src='http://canarytokens.com/traffic/4lizq8u3130qkwuutnva72jhc/post.jsp'/></body></html>")
+    res.setHeader('Content-Type', 'text/html')
+    res.send("<html><head><title>Recovery Codes</title></head><body><img src='http://canarytokens.com/traffic/4lizq8u3130qkwuutnva72jhc/post.jsp'/></body></html>")
 })
 
 
